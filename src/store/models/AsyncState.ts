ad0cc69953594c5b3dc484217fb6a4dc287cdc87
iEPHYP <eric.maulabi@gmail.com>
2023-12-13 20:21:32 +0500

    chore: removed dead code

--- src/store/models/AsyncState.ts
+++
@@ -1,9 +0,0 @@
-export class AsyncState<R> {
-  public loading: boolean = false;
-  public error: boolean = false;
-  public result: R;
-
-  constructor(...inits: Partial<AsyncState<R>>[]) {
-    Object.assign(this, ...inits);
-  }
-}
