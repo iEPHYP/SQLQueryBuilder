f18c88865664c49e236467c5bc628697f134f6bd
iEPHYP <eric.maulabi@gmail.com>
2023-12-13 20:21:20 +0500

    chore: added mock-placeholders

---
+++ src/__tests__/utils/mock-placeholders.ts
@@ -0,0 +1,7 @@
+export const backIconPlaceholder = 'GoBackIcon';
+export const expandMoreIconPlaceholder = 'ExpandMoreIcon';
+export const expandLessIconPlaceholder = 'ExpandLessIcon';
+export const shareIconPlaceholder = 'ShareIcon';
+export const variableIconPlaceholder = 'VariableIcon';
+export const circularProgressLoadingPlaceholder =
+  'CircularProgress is Loading...';
